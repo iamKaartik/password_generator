fea39d1a7224f52a83384aeb09e91f309391887e
Kartik Kumar <61470841+iamKaartik@users.noreply.github.com>
2022-05-14 17:11:25 +0530

    Final Commit

--- main.py
+++ main.py
@@ -1,8 +1,27 @@
-
 from tkinter import *
 from tkinter import messagebox
+from random import shuffle, choice, randint
+import pyperclip
 
+def generate_password():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
+    nr_letters = randint(8, 10)
+    nr_symbols = randint(2, 4)
+    nr_numbers = randint(2, 4)
+
+    password_letters = [choice(letters) for _ in range(randint(8,10))]
+    password_symbols = [choice(symbols) for _ in range(randint(2,4))]
+    password_numbers = [choice(numbers) for _ in range(randint(2,4))]
+    password_list = password_numbers + password_symbols + password_letters
+
+    shuffle(password_list)
+
+    password = "".join(password_list)
+    pyperclip.copy(password)
+    password_entry.insert(0, password)
 
 def save():
 
@@ -11,7 +30,7 @@ def save():
     password = password_entry.get()
 
     if len(password) == 0 or len(website) == 0:
-        messagebox.showwarning(title="Error", message="Please fill all the fields.")
+        messagebox.showwarning(title="OOPs", message="Please fill all the fields.")
 
     else:
         is_ok = messagebox.askokcancel(title=website, message=f"These are the message entered: \nEmail: {email} \nPassword: {password}")
@@ -51,7 +70,7 @@ password_entry = Entry(width=21)
 password_entry.grid(row=3, column=1)
 
 # Buttons
-generate_password_button = Button(text="Generate Password")
+generate_password_button = Button(text="Generate Password", command="generate_password")
 generate_password_button.grid(row=3, column=2)
 add_button = Button(text="Add", width=36, command=save)
 add_button.grid(row=4, column=1, columnspan=2)
